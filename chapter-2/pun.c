2df6e564b2118f7f387b74dc8c88cc0c4419dd09
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-07-21 12:23:25 -0400

    pun.c

---
+++ chapter-2/pun.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // necessary to "include" information about
+                   // C's standard I/O library
+
+int main(void) {   // Executable code goes inside main
+    printf("To C, or not to C: that is the question.\n");
+    return 0; // Program returns 0 to the OS when it terminates
+}
+
