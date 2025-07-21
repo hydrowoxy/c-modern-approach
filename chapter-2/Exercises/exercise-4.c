97e6d27b6e94d7d9d556d8d03bf7dd2fe2f26e88
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-07-21 17:33:19 -0400

    Exercise 4

---
+++ chapter-2/Exercises/exercise-4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b,c,d;
+    float e,f,g,h;
+
+    printf("Ints: \n%d, \n%d, \n%d, \n%d\n",a,b,c,d);
+    printf("-------------------------------\n");
+    printf("Floats: \n%f, \n%f, \n%f, \n%f\n",e,f,g,h);
+
+    return 0;
+}
