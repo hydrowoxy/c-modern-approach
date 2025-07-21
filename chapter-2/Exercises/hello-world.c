4d9031db8505698e4734b711601578c142a28883
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-07-21 17:16:12 -0400

    Exercise 1

---
+++ chapter-2/Exercises/hello-world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    printf("hello, world\n");
+    return 0;
+}
+
+// I didn't get a warning for some reason but I know
+// I should have gotten warned that it says it'll return int
+// but it returns nothing. Fix it by adding the return 0;
