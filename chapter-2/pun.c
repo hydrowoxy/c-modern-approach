91bc147ae08df898b76dfbc37b68a47f3c77fcc0
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-07-21 12:29:25 -0400

    compiling notes

--- chapter-2/pun.c
+++ chapter-2/pun.c
@@ -6,3 +6,29 @@ int main(void) {   // Executable code goes inside main
     return 0; // Program returns 0 to the OS when it terminates
 }
 
+// To compile:
+//      1. preprocessing; program is given to a preprocessor
+//      - which obeys commands that begin with # ("directives")
+//      - preprocessor can add things/modify the program
+//      2. compiling; modified program given to a compiler
+//      - translates it into machine instructions (object code)
+//      3. linking; a linker combines the object code with
+//      - any other code needed to yield a complete exe
+//      - additional code includes library functions like
+//      - printf that are used in the program
+
+// above is usually automated; preprocessor is integrated with compiler
+// use wsl/linux: cc pun.c 
+// linking is automatic using cc.
+
+// cc then leaves an exe in a file named a.out by default.
+// -o flag on cc allows us to choose tha name of the file 
+// containing the exe. so we could say cc -o pun pun.c
+
+// you can also use gcc this is the one we learned in school very similar
+
+// so:
+//      gcc -o pun pun.c
+//      ./pun
+
+// the ./ is important otherwise it's gonna look in system path not this folder
