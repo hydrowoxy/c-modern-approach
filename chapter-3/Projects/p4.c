5e3b9ed8806e0fb7182999300779a872ad4a7ea9
Salma Baig <76014459+hydrowoxy@users.noreply.github.com>
2025-07-24 10:17:25 -0400

    problem 6

---
+++ chapter-3/Projects/p4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b,c;
+
+    printf("Enter phone number [(xxx) xxx-xxxx]: "); 
+    scanf("(%d) %d-%d", &a,&b,&c);
+    printf("You entered %.3d.%.3d.%.4d\n",a,b,c);
+
+    return 0;
+}
